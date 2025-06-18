e4c732b6bacb39b0c483a618fb776388057c5d22
Pablo-R-L <99687142+Pablo-R-L@users.noreply.github.com>
2025-06-18 14:38:04 +0000

    Backend frontend implementation
    Instalação e configuração do concurrently, troca dos data.tsx por data.json, e  criação de uma função para atualizar os dados

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+"use client"
 import { createClient } from '@supabase/supabase-js'
 import { SupabaseWrapper } from './supabase_wrapper'
 import jwt from 'jsonwebtoken';
@@ -13,6 +14,9 @@ import fileUpload from 'express-fileupload';
 import bodyParser from 'body-parser';
 import cors from "cors";
 
+import * as fs from 'fs';
+const raw = fs.readFileSync("frontend/lib/data.json", "utf-8")
+const launchData = JSON.parse(raw)
 
 dotenv.config();
 
@@ -82,6 +86,46 @@ app.listen(process.env.PORT ?? 3000, () => {
 });
 
 
+function setNewLaunch(
+    newName: string,
+    newTarget: string,
+    newTimestamp: string,
+    newAltitude: number,
+    newPosition: number,
+    Velocity: number,
+    launchNumber: number,
+    newAcceleration: number
+    
+) {
+    const newData = {
+        name: newName,
+        target: newTarget,
+        data: []
+    };
+    
+    //while (receber dados do Arduino)
+    setData(newTimestamp, newAltitude, newPosition, Velocity, launchNumber, newAcceleration);
 
+    launchData.push(newData);
+    fs.writeFileSync("frontend/lib/data.json", JSON.stringify(launchData, null, 2), "utf-8")
+}
+
+function setData(
+    newTimestamp: string,
+    newAltitude: number,
+    newPosition: number,
+    Velocity: number,
+    launchNumber: number,
+    newAcceleration: number
+){
+    launchData[launchNumber].data.push({
+        timestamp: newTimestamp,
+        altitude: newAltitude,
+        position: newPosition,
+        velocity: Velocity,
+        acceleration: newAcceleration
+    });
+    fs.writeFileSync("frontend/lib/data.json", JSON.stringify(launchData, null, 2), "utf-8")
+}
 
 
